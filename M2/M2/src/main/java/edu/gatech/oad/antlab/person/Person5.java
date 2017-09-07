e267ab0f596e305fd1b57f2a8dde2d4daf7eaf9b
Varun Subbiah <subbiah.varun@gmail.com>
2017-09-07 13:37:10 -0400

    Added info in AntLabMain, Changed method in person 5, added txt file readme.p5

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,11 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String toRet= "";
+		for (int i = 0; i < input.length(); i++) {
+			toRet  = toRet + ((char)(input.charAt(i)+1));
+		}
+		return toRet;
 	}
 	
 	/**
