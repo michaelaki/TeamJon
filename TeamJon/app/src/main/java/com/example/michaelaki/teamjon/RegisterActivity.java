6db0ef428dcb391cbc984b55ce0f8518be6441f6
Michael Aki <mikeaki901@gmail.com>
2017-09-13 15:39:54 -0400

    Added the TeamJon application.  Currently has a sign in and register option. Wasn't completely sure which things I was supposed to push so it may not run.

---
+++ TeamJon/app/src/main/java/com/example/michaelaki/teamjon/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.michaelaki.teamjon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by michaelaki on 9/13/17.
+ */
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
