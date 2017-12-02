5f50a2eb2da61fe4da6b3ce034c4e23d104a5faf
Jonathan Chen <JonathanChen@c-24-72-151-103.ni.gigamonster.net>
2017-12-02 18:28:11 -0500

    Added push notifications kind of

---
+++ TeamJon/app/src/main/java/com/example/michaelaki/teamjon/model/MyFirebaseMessagingService.java
@@ -0,0 +1,22 @@
+package com.example.michaelaki.teamjon.model;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import android.util.Log;
+
+
+/**
+ * Created by JonathanChen on 12/2/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
